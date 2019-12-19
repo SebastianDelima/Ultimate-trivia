8fd504a2f0ff1e0049b8acb6b9504242679a1137
Sebastian Delima <sebastiandelimagec@gmail.com>
2019-12-19 16:49:06 -0500

    fetching

--- app/controllers/game_sessions_controller.rb
+++ app/controllers/game_sessions_controller.rb
@@ -1,2 +1,7 @@
 class GameSessionsController < ApplicationController
+    def index
+       games =  GameSession.all
+       render json: games
+        end
+
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,8 +5,10 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Subject.destroy_all
+Question.destroy_all
 
 history = Subject.create(title: "History")
 
-history_question1 = Question.create(name:"Who discovered electricity?", answer1:"Ben Franklin", answer2:"Jack Sparrow", answer3:"Nikola Tesla", subject_id: 1)
-history_question2 = Question.create(name:"Who was Emiliano Zapata", answer1:"A brazilian pirate", answer2:"A spanish conquistador", answer3:"A Mexican revolutionary", subject_id: 1)
+history_question1 = Question.create(name:"Who discovered electricity?", answer1:"Ben Franklin", answer2:"Jack Sparrow", answer3:"Nikola Tesla", subject_id: history.id)
+history_question2 = Question.create(name:"Who was Emiliano Zapata", answer1:"A brazilian pirate", answer2:"A spanish conquistador", answer3:"A Mexican revolutionary", subject_id: history.id)
