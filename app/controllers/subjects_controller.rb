9753b3a8a56c5bcad7ec9b6c3f0d39b4d13c60b7
Sebastian Delima <sebastiandelima@Cactus1.local>
2019-12-19 16:49:06 -0500

    fetching

--- app/controllers/subjects_controller.rb
+++ app/controllers/subjects_controller.rb
@@ -1,6 +1,12 @@
 class SubjectsController < ApplicationController
     def index
         subjects = Subject.all
-        render json: subjects
+        render json: subjects.to_json(serialized_data)
     end
+
+    def serialized_data
+        {:except => [:created_at, :updated_at],
+    :include => {:questions => {:except => [:created_at, :updated_at]}}}
+    end
+    
 end
